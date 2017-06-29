29dcfbe597f90ff163d5bca4c5e8165354039083
sergiohdez <sergioluisha@gmail.com>
2017-06-29 13:08:01 +0000

    Config action mailer on production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
-    #domain:               'cs169-1x-sergiohdez.c9users.io',
+    port:                 587,
+    domain:               'cs169-1x-sergiohdez.c9users.io',
     user_name:            'sergioluisha@gmail.com',
     password:             'qsc-lih56',
     authentication:       'plain',
-    enable_starttls_auto: true,
-    ssl: true}
+    enable_starttls_auto: true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
